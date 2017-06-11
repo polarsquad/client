6ed394b4b264ec42101e8c0c8445daeb564eba0f
rhotep <andreas.pittrich@posteo.de>
2017-06-11 18:57:59 +0200

    dsf

--- index_vendor.js
+++ index_vendor.js
@@ -4,8 +4,7 @@ var angular 			= require('angular'),
 	angular_route		= require('angular-sanitize'),
 	angular_translate	= require('angular-translate'),
 	angular_translate	= require('leaflet'),
-	angular_translate	= require('leaflet.markercluster'),
-	qrcode 				= require('qrcode-generator'),
-	ngQrcode 			= require('angular-qrcode')
-
-
+	angular_translate	= require('leaflet.markercluster')
+	// qr_code_utf8		= requirw('node_modules/qrcode-generator/qrcode_UTF8.js'),
+	// qrcode 				= require('node_modules/qrcode-generator/qrcode.js'),
+	// ngQrcode 			= require('angular-qrcode')
